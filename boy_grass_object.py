b5ed53a92b9b9fd4ddb7437609b657221a290aa4
iriring <juju5064@naver.com>
2025-09-30 16:35:13 +0900

    수업 코드

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -14,10 +14,60 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+    def draw(self):
+        self.image.draw(400, 30)
+    def update(self): pass
 
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 90
+        self.frame = random.randint(0, 7)
+        self.image = load_image('run_animation.png')
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+    def draw(self):
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+
+def reset_world():
+    global running
+    global world
+
+    running = True
+    world = []
+
+    grass = Grass()
+    world.append(grass)
+
+    team = [Boy() for i in range(11)]
+    world += team
+
+
+def update_world():
+    for o in world:
+        o.update()
+
+def render_world():
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
 
 
 open_canvas()
+
+reset_world()
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.05)
+
+
+
 
 
 
